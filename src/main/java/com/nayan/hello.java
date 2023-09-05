ae7a01b9f2c3ed4c7e068ff8ec6c6ef28ac4c56b
silentknight2001 <silentknight.www@gmail.com>
2023-09-05 22:20:30 +0530

    added new code

--- src/main/java/com/nayan/hello.java
+++ src/main/java/com/nayan/hello.java
@@ -1,12 +1,15 @@
 package com.nayan;
 public class hello {
     public static void main(String[] args) {
+
         greet();
     }
     static void greet(){
         String hello = "hello this is our first pipeline about devops";
         String him = "Hello world sir";
-
+        int a = 34;
+        int b = 34;
+        System.out.println(a+b);
         System.out.println(hello);
         System.out.print(him);
 
