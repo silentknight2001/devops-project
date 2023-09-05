d7d623f2d11f7a27d308ce6d469eb3713e3ddca9
silentknight2001 <silentknight.www@gmail.com>
2023-09-05 16:50:24 +0530

    version:0.1

---
+++ src/main/java/com/nayan/hello.java
@@ -0,0 +1,15 @@
+package com.nayan;
+public class hello {
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet(){
+        String hello = "hello this is our first pipeline about devops";
+        String him = "Hello world sir";
+
+        System.out.println(hello);
+        System.out.print(him);
+
+    }
+}
+
